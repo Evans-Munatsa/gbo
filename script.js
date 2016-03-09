e860a7e050be587cca5ba449533bded94fa2b8f5
Evans-Munatsa <gagamel.em@gmail.com>
2016-03-09 12:28:53 +0200

    added matching blocks

--- script.js
+++ script.js
@@ -20,10 +20,12 @@ function elements() {
 
   // give random number to all the elements
   for (var i = 0; i < numbers.length; i++) {
-    numbers[i].innerHTML = random(0, 3);
+    numbers[i].innerHTML = random(0, 9);
 
     // make everything green
-    clear();;
+    clear();
+
+    // same row matching
     if (numbers[0].innerHTML === numbers[1].innerHTML && numbers[0].innerHTML ===
       numbers[2].innerHTML && numbers[1].innerHTML === numbers[2].innerHTML) {
       document.getElementsByClassName("number")[0].classList.add("highlight");
@@ -40,6 +42,161 @@ function elements() {
     } else if (numbers[1].innerHTML === numbers[2].innerHTML) {
       document.getElementsByClassName("number")[1].classList.add("highlight");
       document.getElementsByClassName("number")[2].classList.add("highlight");
+    }
+
+
+    if (numbers[3].innerHTML === numbers[4].innerHTML && numbers[3].innerHTML ===
+      numbers[5].innerHTML && numbers[4].innerHTML === numbers[5].innerHTML) {
+      document.getElementsByClassName("number")[3].classList.add("highlight");
+      document.getElementsByClassName("number")[4].classList.add("highlight");
+      document.getElementsByClassName("number")[5].classList.add("highlight");
+    }
+
+    if (numbers[3].innerHTML === numbers[4].innerHTML) {
+      document.getElementsByClassName("number")[3].classList.add("highlight");
+      document.getElementsByClassName("number")[4].classList.add("highlight");
+    } else if (numbers[3].innerHTML === numbers[5].innerHTML) {
+      document.getElementsByClassName("number")[3].classList.add("highlight");
+      document.getElementsByClassName("number")[5].classList.add("highlight");
+    } else if (numbers[4].innerHTML === numbers[5].innerHTML) {
+      document.getElementsByClassName("number")[4].classList.add("highlight");
+      document.getElementsByClassName("number")[5].classList.add("highlight");
+    }
+
+    if (numbers[6].innerHTML === numbers[7].innerHTML && numbers[6].innerHTML ===
+      numbers[8].innerHTML && numbers[7].innerHTML === numbers[8].innerHTML) {
+      document.getElementsByClassName("number")[6].classList.add("highlight");
+      document.getElementsByClassName("number")[7].classList.add("highlight");
+      document.getElementsByClassName("number")[8].classList.add("highlight");
+    }
+
+    if (numbers[6].innerHTML === numbers[7].innerHTML) {
+      document.getElementsByClassName("number")[6].classList.add("highlight");
+      document.getElementsByClassName("number")[7].classList.add("highlight");
+    } else if (numbers[6].innerHTML === numbers[8].innerHTML) {
+      document.getElementsByClassName("number")[6].classList.add("highlight");
+      document.getElementsByClassName("number")[8].classList.add("highlight");
+    } else if (numbers[7].innerHTML === numbers[8].innerHTML) {
+      document.getElementsByClassName("number")[7].classList.add("highlight");
+      document.getElementsByClassName("number")[8].classList.add("highlight");
+    }
+
+    // vertical matching
+    if (numbers[0].innerHTML === numbers[3].innerHTML && numbers[0].innerHTML ===
+      numbers[6].innerHTML && numbers[3].innerHTML === numbers[6].innerHTML) {
+      document.getElementsByClassName("number")[0].classList.add("highlight");
+      document.getElementsByClassName("number")[3].classList.add("highlight");
+      document.getElementsByClassName("number")[6].classList.add("highlight");
+    }
+
+    if (numbers[0].innerHTML === numbers[3].innerHTML) {
+      document.getElementsByClassName("number")[0].classList.add("highlight");
+      document.getElementsByClassName("number")[3].classList.add("highlight");
+    } else if (numbers[0].innerHTML === numbers[6].innerHTML) {
+      document.getElementsByClassName("number")[0].classList.add("highlight");
+      document.getElementsByClassName("number")[6].classList.add("highlight");
+    } else if (numbers[3].innerHTML === numbers[6].innerHTML) {
+      document.getElementsByClassName("number")[3].classList.add("highlight");
+      document.getElementsByClassName("number")[6].classList.add("highlight");
+    }
+
+    if (numbers[2].innerHTML === numbers[5].innerHTML && numbers[2].innerHTML ===
+      numbers[8].innerHTML && numbers[5].innerHTML === numbers[8].innerHTML) {
+      document.getElementsByClassName("number")[2].classList.add("highlight");
+      document.getElementsByClassName("number")[5].classList.add("highlight");
+      document.getElementsByClassName("number")[8].classList.add("highlight");
+    }
+
+    if (numbers[2].innerHTML === numbers[5].innerHTML) {
+      document.getElementsByClassName("number")[2].classList.add("highlight");
+      document.getElementsByClassName("number")[5].classList.add("highlight");
+    } else if (numbers[2].innerHTML === numbers[8].innerHTML) {
+      document.getElementsByClassName("number")[2].classList.add("highlight");
+      document.getElementsByClassName("number")[8].classList.add("highlight");
+    } else if (numbers[5].innerHTML === numbers[8].innerHTML) {
+      document.getElementsByClassName("number")[5].classList.add("highlight");
+      document.getElementsByClassName("number")[8].classList.add("highlight");
+    }
+
+    if (numbers[1].innerHTML === numbers[4].innerHTML && numbers[1].innerHTML ===
+      numbers[7].innerHTML && numbers[4].innerHTML === numbers[7].innerHTML) {
+      document.getElementsByClassName("number")[1].classList.add("highlight");
+      document.getElementsByClassName("number")[4].classList.add("highlight");
+      document.getElementsByClassName("number")[7].classList.add("highlight");
+    }
+
+    if (numbers[1].innerHTML === numbers[4].innerHTML) {
+      document.getElementsByClassName("number")[1].classList.add("highlight");
+      document.getElementsByClassName("number")[4].classList.add("highlight");
+    } else if (numbers[1].innerHTML === numbers[7].innerHTML) {
+      document.getElementsByClassName("number")[1].classList.add("highlight");
+      document.getElementsByClassName("number")[7].classList.add("highlight");
+    } else if (numbers[4].innerHTML === numbers[7].innerHTML) {
+      document.getElementsByClassName("number")[4].classList.add("highlight");
+      document.getElementsByClassName("number")[7].classList.add("highlight");
+    }
+
+    // across matching numbers
+    if (numbers[0].innerHTML === numbers[4].innerHTML && numbers[0].innerHTML ===
+      numbers[8].innerHTML && numbers[4].innerHTML === numbers[8].innerHTML) {
+      document.getElementsByClassName("number")[0].classList.add("highlight");
+      document.getElementsByClassName("number")[4].classList.add("highlight");
+      document.getElementsByClassName("number")[8].classList.add("highlight");
+    }
+
+    if (numbers[0].innerHTML === numbers[4].innerHTML) {
+      document.getElementsByClassName("number")[0].classList.add("highlight");
+      document.getElementsByClassName("number")[4].classList.add("highlight");
+    } else if (numbers[0].innerHTML === numbers[8].innerHTML) {
+      document.getElementsByClassName("number")[0].classList.add("highlight");
+      document.getElementsByClassName("number")[8].classList.add("highlight");
+    } else if (numbers[4].innerHTML === numbers[8].innerHTML) {
+      document.getElementsByClassName("number")[4].classList.add("highlight");
+      document.getElementsByClassName("number")[8].classList.add("highlight");
+    }
+
+    if (numbers[2].innerHTML === numbers[4].innerHTML && numbers[2].innerHTML ===
+      numbers[6].innerHTML && numbers[4].innerHTML === numbers[6].innerHTML) {
+      document.getElementsByClassName("number")[2].classList.add("highlight");
+      document.getElementsByClassName("number")[4].classList.add("highlight");
+      document.getElementsByClassName("number")[6].classList.add("highlight");
+    }
+
+    if (numbers[2].innerHTML === numbers[4].innerHTML) {
+      document.getElementsByClassName("number")[2].classList.add("highlight");
+      document.getElementsByClassName("number")[4].classList.add("highlight");
+    } else if (numbers[2].innerHTML === numbers[6].innerHTML) {
+      document.getElementsByClassName("number")[2].classList.add("highlight");
+      document.getElementsByClassName("number")[6].classList.add("highlight");
+    } else if (numbers[4].innerHTML === numbers[6].innerHTML) {
+      document.getElementsByClassName("number")[4].classList.add("highlight");
+      document.getElementsByClassName("number")[6].classList.add("highlight");
+    }
+
+    // match little
+    if (numbers[0].innerHTML === numbers[5].innerHTML && numbers[2].innerHTML ===
+      numbers[3].innerHTML && numbers[6].innerHTML === numbers[5].innerHTML &&
+      numbers[3].innerHTML === numbers[8].innerHTML) {
+      document.getElementsByClassName("number")[0].classList.add("highlight");
+      document.getElementsByClassName("number")[2].classList.add("highlight");
+      document.getElementsByClassName("number")[3].classList.add("highlight");
+      document.getElementsByClassName("number")[5].classList.add("highlight");
+      document.getElementsByClassName("number")[6].classList.add("highlight");
+      document.getElementsByClassName("number")[8].classList.add("highlight");
+    }
+
+    if (numbers[0].innerHTML === numbers[5].innerHTML) {
+      document.getElementsByClassName("number")[0].classList.add("highlight");
+      document.getElementsByClassName("number")[5].classList.add("highlight");
+    } else if (numbers[2].innerHTML === numbers[3].innerHTML) {
+      document.getElementsByClassName("number")[2].classList.add("highlight");
+      document.getElementsByClassName("number")[3].classList.add("highlight");
+    } else if (numbers[6].innerHTML === numbers[5].innerHTML) {
+      document.getElementsByClassName("number")[6].classList.add("highlight");
+      document.getElementsByClassName("number")[5].classList.add("highlight");
+    } else if (numbers[3].innerHTML === numbers[8].innerHTML) {
+      document.getElementsByClassName("number")[3].classList.add("highlight");
+      document.getElementsByClassName("number")[8].classList.add("highlight");
     }
   }
 }
