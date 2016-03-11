6802eeee0b337d64aa928e3cd27d1c7ce4cbe384
Evans-Munatsa <gagamel.em@gmail.com>
2016-03-11 14:47:35 +0200

    function

--- script.js
+++ script.js
@@ -26,176 +26,153 @@ function elements() {
         start].innerHTML === numbers[start + 2].innerHTML && numbers[start + 1]
       .innerHTML ===
       numbers[start + 2].innerHTML) {
-      document.getElementsByClassName("number")[start].classList.add(
-        "highlight");
-      document.getElementsByClassName("number")[start + 1].classList.add(
-        "highlight");
-      document.getElementsByClassName("number")[start + 2].classList.add(
-        "highlight");
+      number[start].classList.add("highlight");
+      number[start + 1].classList.add("highlight");
+      number[start + 2].classList.add("highlight");
     }
   }
   matchWholeRow(0);
   matchWholeRow(3);
   matchWholeRow(6);
 
-  //first row
+  //first colun
   if (numbers[0].innerHTML === numbers[1].innerHTML) {
-    document.getElementsByClassName("number")[0].classList.add("highlight");
-    document.getElementsByClassName("number")[1].classList.add("highlight");
+    numbers[0].classList.add("highlight");
+    numbers[1].classList.add("highlight");
     displayMessage(numbers[0].innerHTML + " and " + numbers[1].innerHTML +
       " match");
   } else if (numbers[0].innerHTML === numbers[2].innerHTML) {
-    document.getElementsByClassName("number")[0].classList.add("highlight");
-    document.getElementsByClassName("number")[2].classList.add("highlight");
+    numbers[0].classList.add("highlight");
+    numbers[2].classList.add("highlight");
     displayMessage(numbers[0].innerHTML + " and " + numbers[2].innerHTML +
       " match");
   } else if (numbers[1].innerHTML === numbers[2].innerHTML) {
-    document.getElementsByClassName("number")[1].classList.add("highlight");
-    document.getElementsByClassName("number")[2].classList.add("highlight");
+    numbers[1].classList.add("highlight");
+    numbers[2].classList.add("highlight");
     displayMessage(numbers[1].innerHTML + " and " + numbers[2].innerHTML +
       " match");
   }
 
-  //second row
+  //second column
   if (numbers[3].innerHTML === numbers[4].innerHTML) {
-    document.getElementsByClassName("number")[3].classList.add("highlight");
-    document.getElementsByClassName("number")[4].classList.add("highlight");
+    numbers[3].classList.add("highlight");
+    numbers[4].classList.add("highlight");
     displayMessage(numbers[3].innerHTML + " and " + numbers[4].innerHTML +
       " match");
   } else if (numbers[3].innerHTML === numbers[5].innerHTML) {
-    document.getElementsByClassName("number")[3].classList.add("highlight");
-    document.getElementsByClassName("number")[5].classList.add("highlight");
+    numbers[3].classList.add("highlight");
+    numbers[5].classList.add("highlight");
     displayMessage(numbers[3].innerHTML + " and " + numbers[5].innerHTML +
       " match");
   } else if (numbers[4].innerHTML === numbers[5].innerHTML) {
-    document.getElementsByClassName("number")[4].classList.add("highlight");
-    document.getElementsByClassName("number")[5].classList.add("highlight");
+    numbers[4].classList.add("highlight");
+    numbers[5].classList.add("highlight");
     displayMessage(numbers[4].innerHTML + " and " + numbers[5].innerHTML +
       " match");
   }
 
-  //third
+  //third colum
   if (numbers[6].innerHTML === numbers[7].innerHTML) {
-    document.getElementsByClassName("number")[6].classList.add("highlight");
-    document.getElementsByClassName("number")[7].classList.add("highlight");
+    numbers[6].classList.add("highlight");
+    numbers[7].classList.add("highlight");
     displayMessage(numbers[6].innerHTML + " and " + numbers[7].innerHTML +
       " match");
   } else if (numbers[6].innerHTML === numbers[8].innerHTML) {
-    document.getElementsByClassName("number")[6].classList.add("highlight");
-    document.getElementsByClassName("number")[8].classList.add("highlight");
+    numbers[6].classList.add("highlight");
+    numbers[8].classList.add("highlight");
     displayMessage(numbers[6].innerHTML + " and " + numbers[8].innerHTML +
       " match");
   } else if (numbers[7].innerHTML === numbers[8].innerHTML) {
-    document.getElementsByClassName("number")[7].classList.add("highlight");
-    document.getElementsByClassName("number")[8].classList.add("highlight");
+    numbers[7].classList.add("highlight");
+    numbers[8].classList.add("highlight");
     displayMessage(numbers[7].innerHTML + " and " + numbers[8].innerHTML +
       " match");
   }
 
+  //rows
   if (numbers[0].innerHTML === numbers[3].innerHTML) {
-    document.getElementsByClassName("number")[0].classList.add("highlight");
-    document.getElementsByClassName("number")[3].classList.add("highlight");
+    numbers[0].classList.add("highlight");
+    numbers[3].classList.add("highlight");
     displayMessage(numbers[0].innerHTML + " and " + numbers[3].innerHTML +
       " match");
   } else if (numbers[0].innerHTML === numbers[6].innerHTML) {
-    document.getElementsByClassName("number")[0].classList.add("highlight");
-    document.getElementsByClassName("number")[6].classList.add("highlight");
+    numbers[0].classList.add("highlight");
+    numbers[6].classList.add("highlight");
     displayMessage(numbers[0].innerHTML + " and " + numbers[6].innerHTML +
       " match");
   } else if (numbers[3].innerHTML === numbers[6].innerHTML) {
-    document.getElementsByClassName("number")[3].classList.add("highlight");
-    document.getElementsByClassName("number")[6].classList.add("highlight");
+    numbers[3].classList.add("highlight");
+    numbers[6].classList.add("highlight");
     displayMessage(numbers[3].innerHTML + " and " + numbers[6].innerHTML +
       " match");
   }
 
   if (numbers[2].innerHTML === numbers[5].innerHTML) {
-    document.getElementsByClassName("number")[2].classList.add("highlight");
-    document.getElementsByClassName("number")[5].classList.add("highlight");
+    numbers[2].classList.add("highlight");
+    numbers[5].classList.add("highlight");
     displayMessage(numbers[2].innerHTML + " and " + numbers[5].innerHTML +
       " match");
   } else if (numbers[2].innerHTML === numbers[8].innerHTML) {
-    document.getElementsByClassName("number")[2].classList.add("highlight");
-    document.getElementsByClassName("number")[8].classList.add("highlight");
+    numbers[2].classList.add("highlight");
+    numbers[8].classList.add("highlight");
     displayMessage(numbers[2].innerHTML + " and " + numbers[8].innerHTML +
       " match");
   } else if (numbers[5].innerHTML === numbers[8].innerHTML) {
-    document.getElementsByClassName("number")[5].classList.add("highlight");
-    document.getElementsByClassName("number")[8].classList.add("highlight");
+    numbers[5].classList.add("highlight");
+    numbers[8].classList.add("highlight");
     displayMessage(numbers[5].innerHTML + " and " + numbers[8].innerHTML +
       " match");
   }
 
   if (numbers[1].innerHTML === numbers[4].innerHTML) {
-    document.getElementsByClassName("number")[1].classList.add("highlight");
-    document.getElementsByClassName("number")[4].classList.add("highlight");
+    numbers[1].classList.add("highlight");
+    numbers[4].classList.add("highlight");
     displayMessage(numbers[1].innerHTML + " and " + numbers[4].innerHTML +
       " match");
   } else if (numbers[1].innerHTML === numbers[7].innerHTML) {
-    document.getElementsByClassName("number")[1].classList.add("highlight");
-    document.getElementsByClassName("number")[7].classList.add("highlight");
+    numbers[1].classList.add("highlight");
+    numbers[7].classList.add("highlight");
     displayMessage(numbers[1].innerHTML + " and " + numbers[7].innerHTML +
       " match");
   } else if (numbers[4].innerHTML === numbers[7].innerHTML) {
-    document.getElementsByClassName("number")[4].classList.add("highlight");
-    document.getElementsByClassName("number")[7].classList.add("highlight");
+    numbers[4].classList.add("highlight");
+    numbers[7].classList.add("highlight");
     displayMessage(numbers[4].innerHTML + " and " + numbers[7].innerHTML +
       " match");
   }
 
+  //criss cross
   if (numbers[0].innerHTML === numbers[4].innerHTML) {
-    document.getElementsByClassName("number")[0].classList.add("highlight");
-    document.getElementsByClassName("number")[4].classList.add("highlight");
+    numbers[0].classList.add("highlight");
+    numbers[4].classList.add("highlight");
     displayMessage(numbers[0].innerHTML + " and " + numbers[4].innerHTML +
       " match");
   } else if (numbers[0].innerHTML === numbers[8].innerHTML) {
-    document.getElementsByClassName("number")[0].classList.add("highlight");
-    document.getElementsByClassName("number")[8].classList.add("highlight");
+    numbers[0].classList.add("highlight");
+    numbers[8].classList.add("highlight");
     displayMessage(numbers[0].innerHTML + " and " + numbers[8].innerHTML +
       " match");
   } else if (numbers[4].innerHTML === numbers[8].innerHTML) {
-    document.getElementsByClassName("number")[4].classList.add("highlight");
-    document.getElementsByClassName("number")[8].classList.add("highlight");
+    numbers[4].classList.add("highlight");
+    numbers[8].classList.add("highlight");
     displayMessage(numbers[4].innerHTML + " and " + numbers[8].innerHTML +
       " match");
   }
 
   if (numbers[2].innerHTML === numbers[4].innerHTML) {
-    document.getElementsByClassName("number")[2].classList.add("highlight");
-    document.getElementsByClassName("number")[4].classList.add("highlight");
+    numbers[2].classList.add("highlight");
+    numbers[4].classList.add("highlight");
     displayMessage(numbers[2].innerHTML + " and " + numbers[4].innerHTML +
       " match");
   } else if (numbers[2].innerHTML === numbers[6].innerHTML) {
-    document.getElementsByClassName("number")[2].classList.add("highlight");
-    document.getElementsByClassName("number")[6].classList.add("highlight");
+    numbers[2].classList.add("highlight");
+    numbers[6].classList.add("highlight");
     displayMessage(numbers[2].innerHTML + " and " + numbers[6].innerHTML +
       " match");
   } else if (numbers[4].innerHTML === numbers[6].innerHTML) {
-    document.getElementsByClassName("number")[4].classList.add("highlight");
-    document.getElementsByClassName("number")[6].classList.add("highlight");
+    numbers[4].classList.add("highlight");
+    numbers[6].classList.add("highlight");
     displayMessage(numbers[4].innerHTML + " and " + numbers[6].innerHTML +
-      " match");
-  }
-
-  if (numbers[0].innerHTML === numbers[5].innerHTML) {
-    document.getElementsByClassName("number")[0].classList.add("highlight");
-    document.getElementsByClassName("number")[5].classList.add("highlight");
-    displayMessage(numbers[0].innerHTML + " and " + numbers[5].innerHTML +
-      " match");
-  } else if (numbers[2].innerHTML === numbers[3].innerHTML) {
-    document.getElementsByClassName("number")[2].classList.add("highlight");
-    document.getElementsByClassName("number")[3].classList.add("highlight");
-    displayMessage(numbers[2].innerHTML + " and " + numbers[3].innerHTML +
-      " match");
-  } else if (numbers[6].innerHTML === numbers[5].innerHTML) {
-    document.getElementsByClassName("number")[6].classList.add("highlight");
-    document.getElementsByClassName("number")[5].classList.add("highlight");
-    displayMessage(numbers[6].innerHTML + " and " + numbers[5].innerHTML +
-      " match");
-  } else if (numbers[3].innerHTML === numbers[8].innerHTML) {
-    document.getElementsByClassName("number")[3].classList.add("highlight");
-    document.getElementsByClassName("number")[8].classList.add("highlight");
-    displayMessage(numbers[3].innerHTML + " and " + numbers[8].innerHTML +
       " match");
   }
 }
